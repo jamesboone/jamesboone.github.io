3d4b1caa5ac1f80d3e8b413933d3ddc37b8bbbf6
James Boone <jameswboone@gmail.com>
2022-02-18 12:46:06 -0800

    testing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 (function ($) {
-	
+
 	"use strict";
 	$('.owl-men-item').owlCarousel({
 		items:5,
@@ -69,7 +69,7 @@
 	    $("header").removeClass("background-header");
 	  }
 	});
-	
+
 
 	// Window Resize Mobile Menu Fix
 	mobileNav();
@@ -77,11 +77,11 @@
 
 	// Scroll animation init
 	window.sr = new scrollReveal();
-	
+
 
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
-		$(".menu-trigger").on('click', function() {	
+		$(".menu-trigger").on('click', function() {
 			$(this).toggleClass('active');
 			$('.header-area .nav').slideToggle(200);
 		});
@@ -97,8 +97,8 @@
 				var width = $(window).width();
 				if(width < 991) {
 					$('.menu-trigger').removeClass('active');
-					$('.header-area .nav').slideUp(200);	
-				}				
+					$('.header-area .nav').slideUp(200);
+				}
 				$('html,body').animate({
 					scrollTop: (target.offset().top) - 80
 				}, 700);
@@ -109,17 +109,17 @@
 
 	$(document).ready(function () {
 	    $(document).on("scroll", onScroll);
-	    
+
 	    //smoothscroll
 	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
 	        e.preventDefault();
 	        $(document).off("scroll");
-	        
+
 	        $('.scroll-to-section a').each(function () {
 	            $(this).removeClass('active');
 	        })
 	        $(this).addClass('active');
-	      
+
 	        var target = this.hash,
 	        menu = target;
 	       	var target = $(this.hash);
@@ -137,6 +137,7 @@
 	    $('.nav a').each(function () {
 	        var currLink = $(this);
 	        var refElement = $(currLink.attr("href"));
+					var temp = refElement.position();
 	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
 	            $('.nav ul li a').removeClass("active");
 	            currLink.addClass("active");
